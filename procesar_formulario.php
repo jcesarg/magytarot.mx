8f18e62773e30ba2b76a0042aeb4e5c18e6be642
Julio César Pérez García <jcesarg@me.com>
2024-04-29 09:31:40 -0600

    Add files via upload

--- procesar_formulario.php
+++ procesar_formulario.php
@@ -9,11 +9,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $destinatario = 'magy@magytarot.mx';
     
     // Asunto del correo electrónico
-    $asunto = 'Contacto desde sitio web LEKET';
+    $asunto = 'Contacto desde sitio web MAGY TAROT';
 
     // Construir el cuerpo del correo electrónico
     $mensaje = "Nombre: $nombre\n";
-    $mensaje .= "Correo electrónico: $email\n";
+    $mensaje .= "Correo electronico: $email\n";
     $mensaje .= "Telefono: $telefono\n";
     $mensaje .= "Comentarios: $comentarios\n";
 
